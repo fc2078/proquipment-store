b2c2eec5037c938d639fd6d0472a9d6be66ce405
Farhan Chowdury <fchowdury@brooklynsteamcenter.org>
2024-12-09 10:52:37 -0500

    add products and init

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+# All imports
 from flask import Flask, render_template
+import pymysql
+from dynaconf import Dynaconf
 
+# Declare Flash application
 app = Flask(__name__)
+
+conf = Dynaconf(
+    settings_file = ["settings.toml"]
+)
+
+def connect_db():
+    conn = pymysql.connect(
+        host = "10.100.34.80",
+        database = "fchowdury_proquipment_store",
+        user = "fchowdury",
+        password = conf.password,
+        autocommit = True,
+        cursorclass = pymysql.cursors.DictCursor,
+    )
+    return conn
 
 @app.route("/")
 def index():
     return render_template('homepage.html.jinja')
+
+@app.route("/browse")
+def product_browse():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `product` ;")
+    results = cursor.fetchall()
+    return render_template("browse.html.jinja", products = results)
