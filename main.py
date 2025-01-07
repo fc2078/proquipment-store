8e7d7743e9fb9f879f4c04f8ebd61c2d8df37004
Farhan Chowdury <fchowdury@brooklynsteamcenter.org>
2025-01-07 17:09:06 -0500

    overhaul pages and fix funcs

--- main.py
+++ main.py
@@ -4,104 +4,38 @@ import pymysql
 from dynaconf import Dynaconf
 import flask_login
 
-# Declare Flash application
+# Declare Flask application
 app = Flask(__name__)
 
+
+# Config settings
 conf = Dynaconf(
     settings_file = ["settings.toml"]
 )
 
+# Config secret key
 app.secret_key = conf.secret_key
 
+# Establish database connection
 def connect_db():
-    """Connect to database"""
     conn = pymysql.connect(
         host = "10.100.34.80",
         database = "fchowdury_proquipment_store",
         user = "fchowdury",
         password = conf.password,
         autocommit = True,
-        cursorclass = pymysql.cursors.DictCursor,
+        cursorclass = pymysql.cursors.DictCursor
     )
     return conn
 
-@app.route("/")
-def index():
-    return render_template('homepage.html.jinja')
 
-# Browsing page
-@app.route("/browse")
-def product_browse():
-    query = request.args.get('query')
-    conn = connect_db()
-    cursor = conn.cursor()
-    if query is None:
-        cursor.execute("SELECT * FROM `product`;")
-    else:
-        cursor.execute(f"SELECT * FROM `product` WHERE `name` LIKE '%{query}%';")
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return render_template("browse.html.jinja", products = results)
-
-
-
-# Products pages
-@app.route("/product/<product_id>")
-def product_page(product_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM `product` WHERE `id` = {product_id};")
-    result = cursor.fetchone()
-    if result is None:
-        abort(404)
-    cursor.close()
-    conn.close()
-    return render_template("product.html.jinja", product = result)
-
-
-
-# Signup page
-@app.route("/signup", methods=["POST", "GET"])
-def signup_page():
-    if flask_login.current_user.is_authenticated:
-        return redirect("/")
-    else:
-        if request.method == "POST":
-            first_name = request.form["first_name"]
-            last_name = request.form["last_name"]
-            email = request.form["email"]
-            address = request.form["address"]
-            username = request.form["username"]
-            password = request.form["password"]
-            confirm_password = request.form["confirm_password"]
-            conn = connect_db()
-            cursor = conn.cursor()
-            if password != confirm_password:
-                flash("Passwords do not match.")
-            try:
-                cursor.execute(f"""
-                    INSERT INTO `customer`
-                        (`first_name`, `last_name`, `email`, `address`, `username`, `password`, `confirm_password`)
-                    VALUES
-                        ('{first_name}', '{last_name}', '{email}', '{address}', '{username}', '{password}', '{confirm_password}');
-                """)
-            except pymysql.err.IntegrityError:
-                flash("Sorry, that username or email is already taken. Try another.")
-            else:
-                return redirect("/login")
-            finally:
-                cursor.close()
-                conn.close()
-    return render_template("signup.html.jinja")
-
-# Login manager
+# User Login Manager
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "/login"
+login_manager.login_view=("/login")
 
 
-# User class
+# Classes
 class User:
     is_authenticated = True
     is_anonymous = False
@@ -117,143 +51,228 @@ class User:
     def get_id(self):
         return str(self.id)
 
+
+# Load User Session
 @login_manager.user_loader
 def load_user(user_id):
     conn = connect_db()
-    
     cursor = conn.cursor()
-
-    cursor.execute(f"SELECT * FROM `customer` WHERE `id` = {user_id};")
-
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id};")
     result = cursor.fetchone()
-    
-    ##Close Connections
     cursor.close()
     conn.close
-
     if result is not None:
         return User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
 
+
+# Homepage initialization
+@app.route("/")
+def index():
+    return render_template("homepage.html.jinja")
+
+
+# Browse products
+@app.route("/browse")
+def product_browse():
+    query = request.args.get("query")
+    conn = connect_db()
+    cursor = conn.cursor()
+    if query is None:
+        cursor.execute("SELECT * FROM `Product`;")
+    else:
+        cursor.execute(f"SELECT * FROM `Product` WHERE `name` LIKE '%{query}%' OR `description` LIKE '%{query}%' OR `price` LIKE '%{query}%' OR `type` LIKE '%{query}%';")
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template("browse.html.jinja", products = results)
+
+
+##Individual Product Page
+@app.route("/product/<product_id>")
+def product_page(product_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM `Product` WHERE `id` = {product_id};")
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if result is None:
+        abort(404)
+    return render_template("product.html.jinja", product = result)
+        
+
+##Add to Cart Functionality
+@app.route("/product/<product_id>/cart", methods=["POST"])
+@flask_login.login_required
+def add_to_cart(product_id):
+    quantity = request.form["quantity"]
+    customer_id = flask_login.current_user.id
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(f"""
+        INSERT INTO `Cart`
+            (`customer_id`, `product_id`, `quantity`)
+        VALUES
+            ({customer_id}, {product_id}, {quantity})
+        ON DUPLICATE KEY UPDATE
+            `quantity` = `quantity` + {quantity}
+        ;""")
+    cursor.close()
+    conn.close()
+    return redirect("/cart")
+
+
+##Sign Up Page
+@app.route("/signup", methods=["POST", "GET"])
+def signup_page():
+    if flask_login.current_user.is_authenticated:
+        return redirect("/")
+    if request.method == "POST":
+        first_name = request.form["fname"]
+        last_name = request.form["lname"]
+        email = request.form["email"]
+        address = request.form["address"]
+        username = request.form["username"]
+        password = request.form["pass"]
+        confirm_password = request.form["confpass"]
+        conn = connect_db()
+        cursor = conn.cursor()
+        if len(username.strip()) > 20:
+            flash("Username must be 20 characters or less.")
+        else:
+            if len(password.strip()) < 8:
+                flash("Password must be 8 characters or longer.")
+            else:
+                if password != confirm_password:
+                    flash("Passwords do not match.")
+                else:
+                    try:
+                        cursor.execute(f"""
+                            INSERT INTO `Customer`
+                                (`username`, `password`, `first_name`, `last_name`, `email`, `address`)
+                            VALUES
+                                ('{username}', '{password}', '{first_name}', '{last_name}', '{email}', '{address}');
+                        """)
+                    except pymysql.err.IntegrityError:
+                        flash("Username or email is already in use.")
+                    else:
+                        return redirect("/login")
+                    finally:
+                        cursor.close()
+                        conn.close()
+    return render_template("signup.html.jinja")
+
+
+# Login Page
 @app.route("/login", methods=["POST", "GET"])
 def login_page():
     if flask_login.current_user.is_authenticated:
         return redirect("/")
-    else:
-        if request.method == "POST":
-            username = request.form["userVer"].strip()
-            password = request.form["passVer"]
+    if request.method == "POST":
+        username = request.form["userVer"].strip()
+        password = request.form["passVer"]
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM `Customer` WHERE `username` = '{username}';")
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
 
-            conn = connect_db()
+        if result is None:
+            flash("Your username and/or password is incorrect.")
+        
+        elif password != result["password"]:
+            flash("Your username and/or password is incorrect.")
+        
+        else:
+            user = User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
 
-            cursor = conn.cursor()
+            #Loging In
+            flask_login.login_user(user)
 
-            cursor.execute(f"SELECT * FROM `customer` WHERE `username` = '{username}';")
-            
-            result = cursor.fetchone()
+            return redirect("/")
 
-            if result is None:
-                flash("Your username and/or password is incorrect.")
-            
-            elif password != result["password"]:
-                flash("Your username and/or password is incorrect.")
-            
-            else:
-                user = User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+    return render_template("flask_login.html.jinja")
 
-                #Loging In
-                flask_login.login_user(user)
 
-                return redirect("/")
-
-    return render_template("login.html.jinja")
-
-@app.route("/product/<product_id>/cart", methods=["POST"])
+# Cart Page
+@app.route("/cart", methods=["GET"])
 @flask_login.login_required
-def add_to_cart(product_id):
-    """Customer adds product to cart. Login is REQUIRED."""
-    # get quantity from form
-    quantity = request.form["quantity"]
-
-    # check if quantity is a number
-    if not quantity.isdigit():
-        abort(400)
-
-    # check if quantity is positive
-    if int(quantity) <= 0:
-        abort(400)
-
-    # check if product exists
+def cart_page():
     conn = connect_db()
     cursor = conn.cursor()
-
-    cursor.execute(f"SELECT * FROM `product` WHERE `id` = {product_id};")
-
-    result = cursor.fetchone()
-
-    if result is None:
-        abort(404)
-
-    # check if customer has enough stock
-    if result["quantity"] < int(quantity):
-        abort(400)
-
-    # get customer id
-    customer_id = flask_login.current_user.get_id()
-
-    # add data to database
+    customer_id = flask_login.current_user.id
     cursor.execute(f"""
-        INSERT INTO `cart`
-            (`customer_id`, `product_id`, `quantity`)
-        VALUES
-            ({customer_id}, {product_id}, {quantity})
-        ON DUPLICATE KEY UPDATE `quantity` = `quantity` + VALUES(`quantity`);
+        SELECT
+            `name`,
+            `price`,
+            `quantity`,
+            `image`,
+            `product_id`,
+            `Cart`.`id`
+        FROM `Cart` 
+        JOIN `Product` on `product_id` = `Product`.`id` 
+        WHERE `customer_id` = {customer_id};
+    """)
+    results = cursor.fetchall()
+    total = 0
+
+    if len(results) > 0:
+        for item in results:
+            total += (item["price"] * item["quantity"])
+        total = round(total, 2)
+    cursor.close()
+    conn.close()
+    return render_template("cart.html.jinja", cartContents = results, sum = total)
+
+
+# Remove item from Cart
+@app.route("/cart/remove_cart", methods=["POST"])
+@flask_login.login_required
+def remove_cart():
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    customer_id = flask_login.current_user.id
+
+    cart_id = request.form["id"]
+
+    cursor.execute(f"""
+    DELETE FROM `Cart`
+    WHERE
+        `customer_id` = {customer_id}
+    AND
+        `Cart`.`id` = {cart_id};
     """)
 
-    # update product quantity
-    cursor.execute(f"UPDATE `product` SET `quantity` = `quantity` - {quantity} WHERE `id` = {product_id};")
-
-    # commit changes
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-    return redirect("/cart.html.jinja")
-
-    # redirect user to cart page
     
-# Cart setup
-@app.route("/cart")
-@flask_login.login_required
-def cart():
-    conn = connect_db()
-    cursor = conn.cursor()
-    customer_id = flask_login.current_user.get_id()
-    cursor.execute(f"SELECT * FROM `cart` WHERE `customer_id` = {customer_id};")
-    result =  cursor.fetchall()
-    total = 0 
-    if len(result) > 0:
-        for item in result:
-            total += (item["price"] * item["quantity"])
-        total = f"{total:.2f}"
-
     cursor.close()
     conn.close()
+
+    return redirect("/cart")
+
+
+# Update Item Quantity in Cart
+@app.route("/cart/<cart_id>/update", methods=["POST"])
+@flask_login.login_required
+def update_cart(cart_id):
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    new_qty = request.form["upd_quantity"]
+
+    cursor.execute(f"""
+    UPDATE `Cart`
+    SET `quantity` = {new_qty}
+    WHERE `id` = {cart_id}
+    ;""")
+
     
-    return render_template("cart.html.jinja", product = result, sum = total)
-
-# Remove items from cart
-@app.route("/cart/{{['id']}}/remove", methods=["POST"])
-@flask_login.login_required
-def remove_cart(id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    customer_id = flask_login.current_user.get_id()
-    product_id = request.form["product_id"]
-    cursor.execute(f"DELETE FROM `cart` WHERE `customer_id` = {customer_id} `cart_id` = {id};")
-    conn.commit()
     cursor.close()
     conn.close()
+
     return redirect("/cart")
 
 # User logout
